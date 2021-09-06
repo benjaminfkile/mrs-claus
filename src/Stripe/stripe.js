c298811c52ad53d3534076703db85cc2aedbd3aa
benjaminfkile <benjaminfkile@gmail.com>
2021-09-05 19:59:50 -0600

    uses tokens instead of cc data

--- src/Stripe/stripe.js
+++ src/Stripe/stripe.js
@@ -1,64 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
 const jsonParser = express.json()
+let stripe
+if(process.env.TEST === "true"){
+    stripe = require("stripe")(process.env.STRIPE_TEST_KEY);
+}else{
+    stripe = require("stripe")(process.env.STRIPE_KEY);
+}
 
 router
     .route("/createCharge")
     .post(jsonParser, async (req, res) => {
-        const { amount, cardId, oneTime, email } = req.body;
-        if (oneTime) {
-            const {
-                cardNumber,
-                cardExpMonth,
-                cardExpYear,
-                cardCVC,
-                country,
-                postalCode,
-            } = req.body;
-
-            if (!cardNumber || !cardExpMonth || !cardExpYear || !cardCVC) {
-                return res.status(400).send({
-                    Error: "Necessary Card Details are required for One Time Payment",
-                });
-            }
-            try {
-                const cardToken = await stripe.tokens.create({
-                    card: {
-                        number: cardNumber,
-                        exp_month: cardExpMonth,
-                        exp_year: cardExpYear,
-                        cvc: cardCVC,
-                        address_state: country,
-                        address_zip: postalCode,
-                    },
-                });
-
-                const charge = await stripe.charges.create({
-                    amount: amount,
-                    currency: "usd",
-                    source: cardToken.id,
-                    receipt_email: email,
-                    description: `Stripe Charge Of Amount ${amount} for One Time Payment`,
-                });
-
-                if (charge.status === "succeeded") {
-                    return res.status(200).send({ Success: charge });
-                } else {
-                    return res
-                        .status(400)
-                        .send({ Error: "Please try again later for One Time Payment" });
-                }
-            } catch (error) {
-                return res.status(400).send({
-                    Error: error.raw.message,
-                });
-            }
-        } else {
-            return res.status(400).send({
-                Error: "Only One Time Payments Allowed",
-            });
+        console.log(process.env.TEST)
+        try {
+            await stripe.charges.create({
+                amount: parseInt(req.body.amount) * 100,
+                currency: "usd",
+                source: req.body.token.id,
+                receipt_email: "benjaminfkile@gmail.com",
+                description: `Stripe Charge Of Amount ${parseInt(req.body.amount) * 100} for One Time Payment`,
+            }).then(charge => {
+                res.status(200).send({ message: charge })
+            })
+        } catch (err) {
+            res.status(400).send({ message: "bad request" })
+            // console.log(err)
         }
-    });
+    })
 
 module.exports = router
